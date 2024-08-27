adcb0e060a0e8e237b5de2922d4cdd92aeffcd13
Luis Felipe <bl.luisfelipe@gmail.com>
2024-08-26 21:52:52 -0300

    primeiras aulas do leandro

---
+++ segundo.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(void)
+{
+    char nome[35] = "Instituto Federal Sul-Riograndense";
+    char sigla[6] = "IFsul";
+
+    printf("o nome da instituição é: %s\n", nome);
+    printf("a sigla é: %s\n", sigla);
+
+    return 0; // retorna a merda da função
+}
