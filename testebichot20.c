0069a8a564974d68cb96f390ea619e69a60f730b
Luis Felipe <bl.luisfelipe@gmail.com>
2024-09-02 21:53:49 -0300

    começando a avaliação

--- testebichot20.c
+++ testebichot20.c
@@ -19,28 +19,71 @@ typedef struct
 } Monstro;
 
 void mostrar(Monstro bicho, int numero);
+void analisar(Monstro bicho, int numero);
 Monstro criar(int numero);
 
 int main(void)
 {
+
     // objeto Monstro é guardado em 99 variações
-    int y;
-
+    int y = 0;
+    int escolha;
     Monstro A[99];
+    do
+    {
+        // todo esse sistema de switch serve de UI para o usuario levando as funções que ele desejar
+        printf("digite o que deseja fazer: \n 0 -fechar programa \n 1 - criar monstros\n 2 - avaliar monstros \n 3 - mostrar todos os monstros na tela \n");
+        scanf("%i", &escolha);
+        switch (escolha)
+        {
+        case 1:
+            // aqui serve para o usuario digitar a quantidade de monstros que deseja levar, em seguida levando a função de criar os monstros
+            printf("digite a quantidade de Monstros que deseja criar\n");
+            scanf("%i", &y);
+
+            // o for utilizado para a adição dos Monstros com suas informações
+            for (int x = 0; x < y; x++)
+            {
+                A[x] = criar(x + 1);
+            }
+            break;
+
+        case 2:
+            for (int x = 0; x < y; x++)
+            {
+                analisar(A[x], x + 1);
+            }
+            break;
+
+        case 3:
+            if (y != 0)
+            {
+                for (int z = 0; z < y; z++)
+                {
+                    mostrar(A[z], z + 1);
+                };
+                printf("\ntodos os monstros mostrados.");
+            }
+            else
+            {
+                printf("você não criou nenhum monstro, por favor adicione!\n");
+            }
+            break;
+
+        default:
+            if (escolha == 0)
+            {
+                printf("fechando programa.....");
+            }
+            else
+            {
+                printf("escolha invalida, tente novamente:\n");
+            }
+        }
+    } while (escolha != 0);
     // aqui pergunta a quantidade de Monstros que se deseja avaliar, sendo utilizada no for
-    printf("digite a quantidade de Monstros que deseja avaliar\n");
-    scanf("%i", &y);
 
-    // o for utilizado para a adição dos Monstros com suas informações
-    for (int x = 0; x < y; x++)
-    {
-        A[x] = criar(x + 1);
-    }
-
-    for (int z = 0; z < y; z++)
-    {
-        mostrar(A[z], z + 1);
-    }
+    return 0;
 }
 
 void mostrar(Monstro m, int numero)
@@ -61,12 +104,13 @@ void mostrar(Monstro m, int numero)
 
     printf("\n ele tem  %i de bonus de acerto, dando um dano medio de %f\n de resistencias ele possui %i de resistencia forte, %i de resistencia media e %i de resistencia fraca\n \n", m.Vatq, m.dano, m.resFo, m.resM, m.resFr);
 
-    if (strcmp(m.CD, "") == 0)
+    if (m.CD != 0)
     {
-        printf("sua CD de efeito é %i", m.CD);
+        {
+            printf("sua CD de efeito é %i", m.CD);
+        }
     }
-
-    if (strcmp(m.CD, "") == 0)
+    if (m.PM != 0)
     {
         printf("possuindo %i de PM's", m.PM);
     }
@@ -78,15 +122,28 @@ void mostrar(Monstro m, int numero)
 Monstro criar(int x)
 {
     Monstro m;
-    // pérgunta e respostas são variaveis feitas para guardar as respostas do usuario
+    // pergunta e respostas são variaveis feitas para guardar as respostas do usuario, no caso do tipo e nivel do monstro são numeros que tem um intervalo definido o programa se nega a analisar algo diferente do necessario
     char pergunta[4];
     int resposta = 0;
     printf("digite nome do Monstro %i \n", x);
     scanf("%s", m.nome);
+    do
+    {
+        printf("você quer colocar alguma ND na criatura %i , se sim escreva ''sim'', do contrario escreva 'nao' \n", x);
+
+        scanf("%s", pergunta);
+    } while (strcmp(pergunta, "sim") != 0 && strcmp(pergunta, "nao") != 0);
+    if (strcmp(pergunta, "sim") == 0)
+    {
+        do
+        {
+            printf("digite o nivel de desafio do Monstro, se deseja nd S escreva 21, S+ 22 %i \n", x);
+            scanf("%f", &m.ND);
+        } while (m.ND == 0.25 && m.ND == 0.5 || m.ND < 1 && m.ND > 22);
+    }
     printf("digite o valor de ataque do Monstro %i \n", x);
     scanf("%i", &m.Vatq);
-    printf("digite o nivel de desafio do Monstro %i \n", x);
-    scanf("%f", &m.ND);
+
     printf("digite a media de dano do Monstro %i \n", x);
     scanf("%f", &m.dano);
 
@@ -104,9 +161,11 @@ Monstro criar(int x)
 
     printf("digite os pontos de vida da criatura %i \n", x);
     scanf("%i", &m.pv);
-
-    printf("digite o tipo de monstro: 1-solo, 2-lacaio e 3-especial\n", x);
-    scanf("%i", &m.tipo);
+    do
+    {
+        printf("digite o tipo de monstro: 1-solo, 2-lacaio e 3-especial\n", x);
+        scanf("%i", &m.tipo);
+    } while (m.tipo > 1 && m.tipo < 3);
     // ambos esses do-while servem para perguntar se o usuario quer adicionar alguma coisa opcional, caso queira pede para adicionar, do contrario ele preenche com um nulo.
     do
     {
@@ -123,9 +182,11 @@ Monstro criar(int x)
     }
     else
     {
-        m.CD = "";
+        m.CD = 0;
     }
+    // reinicia a variavel pergunta
     pergunta == "";
+    // mesma coisa da CD
     do
     {
         printf("você quer colocar PM's na criatura %i , se sim escreva ''sim'', do contrario escreva 'nao'  \n", x);
@@ -141,8 +202,24 @@ Monstro criar(int x)
     }
     else
     {
-        m.PM = "";
+        m.PM = 0;
     }
 
     return m;
+}
+
+void analisar(Monstro bicho, int numero)
+{
+    printf("entrou");
+    float calc = 0;
+    int temp = 6;
+    switch (temp)
+    {
+    case 4:
+        calc = +0.25;
+    case 5 ... 30:
+        printf("foi");
+    default:
+        printf("não foi");
+    }
 }
